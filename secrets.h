3933c8d3ea70a03bc54dffbc9489f80bd9b48132
zmuzsik <zoltan.muzsik@gmail.com>
2022-02-20 17:10:58 +0100

    Removed personal info

--- secrets.h
+++ secrets.h
@@ -1,9 +1,9 @@
 // Replace with your WiFi details
-#define WIFI_SSID   "Z3R0"
-#define WIFI_PASSWD "HCCM0XXY"
+#define WIFI_SSID   "SSID"
+#define WIFI_PASSWD "PASSWORD"
 
 // Replace with your MQTT broker details
 #define MQTT_PORT   1883
-#define MQTT_SERVER "192.168.1.4"
+#define MQTT_SERVER "Server IP"
 #define MQTT_USER   ""
 #define MQTT_PASSWD ""
